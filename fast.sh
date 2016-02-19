9b6d2a76a821c12be5cb791007292fda87ddba1b
mrmacsi <macitsimsek12@hotmail.com>
2016-02-19 19:19:16 +0200

    Deneme
    Deneme

---
+++ fast.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'Updating'
+apt-get update 
+echo 'Upgrade'
+apt-get upgrade
+y
+echo 'Ngix Install and Php Install'
+apt-get install nginx php5-fpm php5-cli php5-mcrypt git
+sudo apt-get install php5-gd
+php -i | grep -i gd
