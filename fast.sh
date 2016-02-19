f242d9c92d02aa0387a1bb765b6325526a4cfd3a
mrmacsi <macitsimsek12@hotmail.com>
2016-02-19 19:57:03 +0200

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 apt-get update  # To get the latest package lists
-apt-get upgrade <package name> -y
-y
+apt-get -y upgrade [packagename]
 apt-get install nginx php5-fpm php5-cli php5-mcrypt git
 sudo apt-get install php5-gd
 php -i | grep -i gd
