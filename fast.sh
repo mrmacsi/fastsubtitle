f479f62910a57ffde31982031b60019fe1f9d0d3
mrmacsi <macitsimsek12@hotmail.com>
2017-01-21 17:34:15 +0200

    Update fast.sh
    php 7

--- fast.sh
+++ fast.sh
@@ -1,69 +1,39 @@
-#!/bin/bash
-echo '***********************************'
-echo '**************Updating*************'
-echo '***********************************'
-sudo apt-get update
-sudo apt-get -y install nginx php5-fpm php5-cli php5-mcrypt git php5-gd
-clear 
-echo '***********************************'
-echo '***********Php İnstall************'
-echo '***********************************'
-curl -O https://raw.githubusercontent.com/mrmacsi/fastsubtitle/master/php.ini
-mv php.ini /etc/php5/fpm/
-sudo php5enmod mcrypt
-sudo service php5-fpm restart
-clear 
-echo '***********************************'
-echo '*************Show Grep*************'
-echo '***********************************'
-php -i | grep -i gd
-sudo mkdir -p /var/www/laravel
-curl -O https://raw.githubusercontent.com/mrmacsi/fastsubtitle/master/default
-mv default /etc/nginx/sites-available/
-clear 
-echo '***********************************'
-echo '************Restarting*************'
-echo '***********************************'
-sudo service nginx restart
-clear 
-echo '***********************************'
-echo '*************Composer**************'
-echo '***********************************'
-cd ~
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-clear 
-echo '***********************************'
-echo '**************GitLab***************'
-echo '***********************************'
-echo '***********************************'
-echo '***********Laravel 5.2*************'
-echo '***********************************'
-composer create-project --prefer-dist laravel/laravel /var/www/laravel/ "5.2.*"
-sudo chown -R :www-data /var/www/laravel
-sudo chmod -R 775 /var/www/laravel/storage
-cd /var/www/laravel
-curl -O https://raw.githubusercontent.com/mrmacsi/fastsubtitle/master/.env
-composer install
-clear 
-echo '***********************************'
-echo '**************Mysql****************'
-echo '***********************************'
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get install -y language-pack-en-base
+sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
 echo "mysql-server-5.5 mysql-server/root_password password 123984" | debconf-set-selections
 echo "mysql-server-5.5 mysql-server/root_password_again password 123984" | debconf-set-selections
-apt-get -y install mysql-server-5.5
-apt-get -y install php5-mysql
-clear
-echo '***********************************'
-echo '*************Database**************'
-echo '***********************************'
-mysql --host="localhost" --user=root --password=123984  -e "create database fastsubtitle;"
+sudo apt-get -y update && apt-get -y --purge autoremove && apt-get -y install git nginx php7.0-fpm  php7.0-mysql php7.0-cli php7.0-curl php7.0-gd php7.0-json php7.0-mcrypt php7.0-xml php7.0-mbstring zip unzip php7.0-zip mysql-server-5.5
+service php7.0-fpm restart
+service nginx restart
 sudo chmod -R gu+w /var/www/
 sudo chmod -R guo+w /var/www/
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+git clone https://gitlab.com/salvadorx/fastsubtitle.git /var/www/laravel
+sudo chown -R :www-data /var/www/laravel
+sudo chmod -R 777 /var/www/laravel/storage
+sudo chmod -R 777 /var/www/laravel/bootstrap
+curl -O https://raw.githubusercontent.com/mrmacsi/fastsubtitle/master/.env
+sudo mv .env /var/www/laravel
+composer install
+mysql --host="localhost" --user=root --password=123984  -e "create database fastsubtitle;"
+curl -O https://raw.githubusercontent.com/mrmacsi/fastsubtitle/master/default
+mv default /etc/nginx/sites-available/
+sudo chown $(whoami):www-data /var/www/laravel/. -R
+sudo chown www-data: /var/www/laravel/storage -R
+chmod -R 777 /var/www/laravel/public/backend/peopleImages/
+chmod -R 777 /var/www/laravel/public/backend/videoImages/
+/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+/sbin/mkswap /var/swap.1
+/sbin/swapon /var/swap.1
+composer update
+php artisan cache:clear
+php artisan optimize
+php artisan migrate
+service php7.0-fpm restart
+service nginx restart
 clear
 echo '***********************************'
-echo '**********Composer Update**********'
+echo '**********Setup Complated**********'
 echo '***********************************'
-cd ..
-cd var/www/laravel
-composer require yandex/translate-api
