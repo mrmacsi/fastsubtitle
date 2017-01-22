dc37d58ffa05b90306d1a7f551203b3952c5136b
mrmacsi <macitsimsek12@hotmail.com>
2017-01-22 11:03:16 +0200

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -21,6 +21,8 @@ composer install
 mysql --host="localhost" --user=root --password=123984  -e "create database fastsubtitle;"
 curl -O https://raw.githubusercontent.com/mrmacsi/fastsubtitle/master/default
 mv default /etc/nginx/sites-available/
+curl -O https://raw.githubusercontent.com/mrmacsi/fastsubtitle/master/php7/php.ini
+mv php.ini /etc/php/7.0/
 sudo chown $(whoami):www-data /var/www/laravel/. -R
 sudo chown www-data: /var/www/laravel/storage -R
 chmod -R 777 /var/www/laravel/public/backend/peopleImages/
